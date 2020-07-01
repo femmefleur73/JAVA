14c2f4b8c00379da169cd189469ea3934a82968a
femmefleur73 <femmefleur73@gmail.com>
2020-07-01 23:50:53 +0900

    11111

---
+++ WCF_RESTful/WCFRESTServiceDemo/IProductService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFRESTServiceDemo
+{
+    [ServiceContract]
+    public interface IProductService
+    {
+
+        [OperationContract]
+        [WebGet(UriTemplate = "/GetProduct/{productId}",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped)]
+        List<Product> GetProductDetails(string productId);
+
+        [OperationContract]
+        [WebInvoke(UriTemplate = "GetProduct",
+                   Method = "POST",
+                   BodyStyle = WebMessageBodyStyle.WrappedRequest,
+                   RequestFormat = WebMessageFormat.Json,
+                   ResponseFormat = WebMessageFormat.Json)]
+        Product GetProduct(string product);
+    }
+
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCost { get; set; }
+    }
+
+}
